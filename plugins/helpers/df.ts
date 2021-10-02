a5c239f19662a884bbdcb2cae96b97660ba95bbe
cha0sg0d <tony.goss@protonmail.com>
2021-10-02 18:26:32 -0400

    basic helpers

--- plugins/helpers/df.ts
+++ plugins/helpers/df.ts
@@ -1,7 +1,7 @@
 import { COLOSSUS_ABI } from "../generated/abi";
 import { COLOSSUS_ADDRESS } from "../generated/contract";
 import { DaoContractPlayer } from "../types";
-import { Planet } from "@darkforest_eth/types";
+import { Planet, PlanetType } from "@darkforest_eth/types";
 
 // @ts-expect-error
 const { getPlanetName, getPlayerColor } = df.getProcgenUtils();
@@ -52,4 +52,55 @@ export function getPlanetByLocationId(locationId: Planet) {
 export function getBlockNumber() {
   // @ts-expect-error
   return df.ethConnection.blockNumber;
+}
+
+export const energy = (planet: Planet) => {
+  return Math.floor(planet.energy / planet.energyCap * 100);
+}
+
+export const isFoundry = (planet:Planet) => {
+  return planet.planetType == PlanetType.RUINS;
+}
+
+export const canHaveArtifact = (planet: Planet) => {
+  return isFoundry(planet) && !planet.hasTriedFindingArtifact
+}
+
+export const enoughEnergyToProspect = (planet:Planet) => {
+  return energy(planet) >= 96;
+};
+
+export const print = (msg: string) => {
+  // @ts-expect-error
+  df.terminal.current.println(msg);
+}
+
+/* reads on-chain data to confirm */
+export const isFindable = (planetDetails: any, currentBlockNumber: number) => {
+    const pName = getPlanetName(planetDetails[0][0]);
+    print(`examining ${pName}`);
+    const prospectedBlockNumber = planetDetails[1][10];
+    const hasTriedFindingArtifact = planetDetails[1][9];
+    print(`prospected # ${prospectedBlockNumber}\nalready tried to find? ${hasTriedFindingArtifact}`)
+    return (
+      prospectedBlockNumber !== 0 &&
+      !hasTriedFindingArtifact
+      // !planet.unconfirmedFindArtifact
+      // !prospectExpired(currentBlockNumber, planet.prospectedBlockNumber)
+    );
+  }
+  
+export const isProspectable = (planet: Planet) => {
+    return isFoundry(planet) && enoughEnergyToProspect(planet) && planet.prospectedBlockNumber === undefined && !planet.unconfirmedProspectPlanet;
+}
+
+export const getProspectablePlanets = async (planets: Planet[]) => {
+  let prospectablePlanets = planets.filter(isProspectable);  
+  return prospectablePlanets;
+}
+
+export const bulkUiRefresh = async (planets: Planet[]) => {
+  const locationIds = planets.map((p) => p.locationId)
+  // @ts-expect-error
+  await df.bulkHardRefreshPlanets(locationIds);
 }
