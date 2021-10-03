a38f2c868d5ee5f9e5a205237682ca5cbbdaea8c
cha0sg0d <tony.goss@protonmail.com>
2021-10-03 11:35:50 -0400

    add gas price

--- plugins/hooks/use-player.ts
+++ plugins/hooks/use-player.ts
@@ -1,9 +1,41 @@
 import { ethers } from "ethers";
 
-export const usePlayer = () => {
-  const url = "http://165.227.93.253:8545";
-  const provider = new ethers.providers.JsonRpcProvider(url);
+const CORE_CONTRACT_ADDRESS = '0x27a166aE00C33Bef64306760aCd7C9fD3c2fEB74'
 
+const GAS_KEY = 'GasFeeGwei';
+
+export function getLocalStorageSettingKey(
+  account: any | undefined,
+  setting: any
+): string {
+  if (account === undefined) {
+    return CORE_CONTRACT_ADDRESS + ':anonymous:' + setting;
+  }
+
+  return CORE_CONTRACT_ADDRESS + ':' + account + ':' + setting;
+}
+
+export function getSetting(account: any | undefined, setting: any): string {
+  const key = getLocalStorageSettingKey(account, setting);
+
+  // console.log(`key for localStorage`, key);
+  let valueInStorage = window.localStorage.getItem(key);
+
+  if (valueInStorage === null) {
+    valueInStorage = '10'; /* high lol */
+  }
+  // console.log(`gas ⛽️`, valueInStorage);
+  return valueInStorage;
+}
+
+export const useGasPrice = (): string => {
+  // @ts-expect-error
+  return getSetting(df.account, GAS_KEY);
+}
+
+export const usePlayer = () => {
+  // @ts-expect-error
+  const provider = df.ethConnection.provider;
   // @ts-expect-error
   const wallet = new ethers.Wallet(df.getPrivateKey(), provider);
 
